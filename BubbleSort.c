04cf1ea8dced4f27b5a0801451a8c8f0e57f7564
khushal <111033014+khushaldhola@users.noreply.github.com>
2022-09-29 19:18:01 +0530

    Update BubbleSort.c

--- BubbleSort.c
+++ BubbleSort.c
@@ -1,20 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
+void makeArray(int arr[], int size)
 {
-    int size;
-    printf("enter the value of array size :- ");
-    scanf("%d",&size);
-    int arr[size];
-
-
     for (int i = 0; i < size; i++)
     {
         printf("enter arr[%d] :- ", i + 1);
         scanf("%d", &arr[i]);
     }
-    // Sorting
+}
+void bubbleSort(int arr[], int size)
+{
     for (int i = 0; i < size; i++)
     {
         for (int j = i + 1; j < size; j++)
@@ -28,7 +24,21 @@ int main()
             }
         }
     }
-    // Display
+}
+void printArray(int arr[], int size)
+{
     for (int i = 0; i < size; i++)
         printf("\narr[%d] = %d", i + 1, arr[i]);
+}
+
+int main()
+{
+    int size;
+    printf("enter the value of array size :- ");
+    scanf("%d", &size);
+    int arr[size];
+
+    makeArray(arr, size);
+    bubbleSort(arr, size);
+    printArray(arr, size);
 }
